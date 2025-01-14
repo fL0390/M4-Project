fb185fb3473e06ecbb11e8e9b779ea569ced3a79
fL03 <91798342+fL0390@users.noreply.github.com>
2025-01-14 16:42:24 +0100

    Cambiado error login y Main

--- scripts/login.js
+++ scripts/login.js
@@ -2,23 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector(".form");
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
+    const messageContainer = document.querySelector(".incorrect");
 
     form.addEventListener("submit", (event) => {
-        event.preventDefault(); // Evita que el formulario se envíe automáticamente
+        event.preventDefault();
 
         const username = usernameInput.value.trim();
         const password = passwordInput.value.trim();
 
-        // Buscar el usuario en la lista
         const user = users.find(user => user.username === username && user.password === password);
 
         if (user) {
-            // Si las credenciales son correctas, redirigir al usuario
-            alert(`Bienvenido, ${user.username}!`);
-            window.location.href = "./sites/main.html"; // Redirige al sitio principal
+            messageContainer.style.display = 'none'
+            window.location.href = "./sites/main.html"; 
         } else {
-            // Si las credenciales son incorrectas, mostrar un mensaje de error
-            alert("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.");
+            messageContainer.style.display = 'block'; 
         }
     });
 });
